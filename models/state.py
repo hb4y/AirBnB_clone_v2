0b8093faaadefae0d29b18da91dfa0e26c32560f
Gonzalo Gomez Millan <1240@holbertonschool.com>
2020-04-01 22:54:46 +0000

    Adding atributes and mothods to State class

--- models/state.py
+++ models/state.py
@@ -3,9 +3,22 @@
 from models.base_model import BaseModel
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """This is the class for State
     Attributes:
         name: input name
     """
-    name = ""
+    __tablename__ = "states"
+    name = Column(String(128), nullable=False)
+    cities = relationship("City",  backref="state", cascade="delete")
+
+    if getenv("HBNB_TYPE_STORAGE") == "file":
+        @property
+        def cities(self):
+            """Get the cities form the state.
+            """
+            cl = []
+            for city in list(models.storage.all(City).values()):
+                if city.state_id == self.id:
+                    cl.append(city)
+            return cl
