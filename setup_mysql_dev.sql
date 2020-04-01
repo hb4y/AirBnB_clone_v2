d98a2a57a3453d8e02e14aad0bbb361a2d4ac45e
Gonzalo Gomez Millan <1240@holbertonschool.com>
2020-04-01 07:21:24 +0000

    Fix username and dbname for the MySQL dev script

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -2,10 +2,12 @@
 --   creates the database hbnb_dev_db.
 --   creates the user hbnb_dev.
 -- Create database
-CREATE DATABASE IF NOT EXISTS hbtn_dev_db;
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+DROP USER IF EXISTS 'hbnb_dev'@'localhost';
+FLUSH PRIVILEGES;
 -- Create user
-CREATE USER IF NOT EXISTS 'user_dev'@'localhost' IDENTIFIED BY 'user_dev_pwd';
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 -- Grant all
-GRANT ALL ON hbnb_dev_db.* TO 'user_dev'@'localhost';
+GRANT ALL ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
 -- Grant select
-GRANT SELECT ON performance_schema.* TO 'user_dev'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
