1c25e201eaf09be2b6d2058f1ccb437c78a556fc
Gonzalo Gomez Millan <1240@holbertonschool.com>
2020-04-01 22:20:19 +0000

    Updating attributes for City class

--- models/city.py
+++ models/city.py
@@ -1,13 +1,17 @@
 #!/usr/bin/python3
 """This is the city class"""
+from sqlalchemy import Column
+from sqlalchemy import ForeignKey
+from sqlalchemy import String
 from models.base_model import BaseModel
 
 
-class City(BaseModel):
+class City(BaseModel, Base):
     """This is the class for City
     Attributes:
         state_id: The state id
         name: input name
     """
-    state_id = ""
-    name = ""
+    __tablename__ = "cities"
+    state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
+    name = Column(String(128), nullable=False)
