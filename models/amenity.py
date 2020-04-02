70f6912d1080494dd0c478776b75494f8c8b6fb6
Gonzalo Gomez Millan <1240@holbertonschool.com>
2020-04-02 17:22:20 -0500

    Adding DBStorage to Amenity and the Many-to-Many

--- models/amenity.py
+++ models/amenity.py
@@ -5,11 +5,19 @@ from models.base_model import BaseModel
 from sqlalchemy import Column
 from sqlalchemy import String
 from sqlalchemy.orm import relationship
+from models.place import Place
 
 
-class Amenity(BaseModel):
+class Amenity(BaseModel, Base):
     """This is the class for Amenity
+
     Attributes:
+        __tablename__ (str): The name of the MySQL table to store Amenities.
+        name (sqlalchemy String): The amenity name.
+        place_amenities (sqlalchemy relationship): Place-Amenity relationship.
         name: input name
     """
+    __tablename__ = "amenities"
     name = Column(String(128), nullable=False)
+    place_amenities = relationship("Place", secondary="place_amenity",
+                                   viewonly=False)
