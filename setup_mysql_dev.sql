1a40643c6203b2e03e33cb17421733cf0cece8d5
Gonzalo Gomez Millan <1240@holbertonschool.com>
2020-04-02 08:15:11 +0000

    Remove comments from MySQL script

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,5 +1,8 @@
 -- Script that prepares a MySQL server for the project:
 -- creates the database hbnb_dev_db. Creates the user hbnb_dev.
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+DROP USER IF EXISTS hbnb_dev@localhost;
+FLUSH PRIVILEGES;
+CREATE USER IF NOT EXISTS hbnb_dev@localhost IDENTIFIED BY 'hbnb_dev_pwd';
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,5 +1,8 @@
 -- Script that prepares a MySQL server for the project:
 --   creates the database hbnb_test_db.  Creates the user hbnb_test.
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+DROP USER IF EXISTS hbnb_test@localhost;
+FLUSH PRIVILEGES;
+CREATE USER IF NOT EXISTS hbnb_test@localhost IDENTIFIED BY 'hbnb_test_pwd';
 GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
 GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
