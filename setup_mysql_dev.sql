b0c8e25c5b3cf154863f27e35f5487d9bdd84a80
Gonzalo Gomez Millan <1240@holbertonschool.com>
2020-04-02 06:48:45 +0000

    Fix sql code

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,13 +1,7 @@
 -- Script that prepares a MySQL server for the project:
---   creates the database hbnb_dev_db.
---   creates the user hbnb_dev.
--- Create database
+-- creates the database hbnb_dev_db. Creates the user hbnb_dev.
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
-DROP USER IF EXISTS 'hbnb_dev'@'localhost';
+CREATE USER IF NOT EXISTS hbnb_dev@localhost IDENTIFIED BY hbnb_dev_pwd;
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO hbnb_dev@localhost;
+GRANT SELECT ON performance_schema.* TO hbnb_dev@localhost;
 FLUSH PRIVILEGES;
--- Create user
-CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
--- Grant all
-GRANT ALL ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
--- Grant select
-GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,13 +1,7 @@
 -- Script that prepares a MySQL server for the project:
---   creates the database hbnb_test_db.
---   creates the user hbnb_test.
--- Create database
+--   creates the database hbnb_test_db.  Creates the user hbnb_test.
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
-DROP USER IF EXISTS 'hbnb_test'@'localhost';
+CREATE USER IF NOT EXISTS hbnb_test@localhost IDENTIFIED BY hbnb_test_pwd;
+GRANT ALL ON hbnb_test_db.* TO hbnb_test@localhost;
+GRANT SELECT ON performance_schema.* TO hbnb_test@localhost;
 FLUSH PRIVILEGES;
--- Create user
-CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
--- Grant all
-GRANT ALL ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
--- Grant select
-GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
