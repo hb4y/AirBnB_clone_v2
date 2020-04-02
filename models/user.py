cfd590d94f3c3b0c99b2b471a5fcf9f28a833276
Gonzalo Gomez Millan <1240@holbertonschool.com>
2020-04-02 15:38:12 +0000

    Update class Place and User to DBStore

--- models/user.py
+++ models/user.py
@@ -1,9 +1,14 @@
 #!/usr/bin/python3
 """This is the user class"""
+from models.base_model import Base
 from models.base_model import BaseModel
+from sqlalchemy import Column
+from sqlalchemy import String
+from models.place import Place
+from sqlalchemy.orm import relationship
 
 
-class User(BaseModel):
+class User(BaseModel, Base):
     """This is the class for user
     Attributes:
         email: email address
@@ -11,7 +16,10 @@ class User(BaseModel):
         first_name: first name
         last_name: last name
     """
-    email = ""
-    password = ""
-    first_name = ""
-    last_name = ""
+    __tablename__ = "users"
+
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128), nullable=True)
+    last_name = Column(String(128), nullable=True)
+    places = relationship("Place", backref="user", cascade="all, delete")
