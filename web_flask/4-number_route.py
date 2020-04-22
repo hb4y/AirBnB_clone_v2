4402306d5f3bc86718412f89a44e9e027635509a
hb4y <hugobaymon@gmail.com>
2020-04-22 23:13:38 +0000

    [task 4] is a number

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""flask app"""
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_world():
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    return 'HBNB'
+
+
+@app.route('/c/<string:text>', strict_slashes=False)
+def c(text):
+    return 'C ' + text.replace("_", " ")
+
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<string:text>', strict_slashes=False)
+def python_string(text="is cool"):
+    return 'Python ' + text.replace("_", " ")
+
+
+@app.route('/python/<int:n>', strict_slashes=False)
+def number(n):
+    return str(n) + " is a number"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
